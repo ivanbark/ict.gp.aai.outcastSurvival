6374c4158e9398897e7e6a12e56e317ef86c446b
Casper Schouwenaar <casper.schouwenaar2001@gmail.com>
2025-02-10 14:45:24 +0100

    player controls basics

--- Player.cs
+++ Player.cs
@@ -14,15 +14,37 @@ public partial class Player : MovingEntity
   {
     // implement seek for now
 
-    Vector2 DesiredVelocity = (World_ref.Target - Position) * MaxSpeed;
-    Vector2 steeringForce = DesiredVelocity - Velocity;
+    // Vector2 DesiredVelocity = (World_ref.Target - Position) * MaxSpeed;
+    // Vector2 steeringForce = DesiredVelocity - Velocity;
 
-    Vector2 acceleration = steeringForce / Mass;
+    // Vector2 acceleration = steeringForce / Mass;
 
-    Velocity += acceleration * (float) delta;
-    // truncate the velocity to the maxspeed
+    // Velocity += acceleration * (float) delta;
+    // // truncate the velocity to the maxspeed
 
 
-    Position += Velocity * (float) delta;
+    // Position += Velocity * (float) delta;
+
+    var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    if(Input.IsActionPressed("move_up")) {
+      Position += Vector2.Up * 10;
+      animatedSprite2D.Animation = "up";
+      Rotation = (float)Math.PI;
+    }
+
+    if(Input.IsActionPressed("move_down")) {
+      Position += Vector2.Down * 10;
+      Rotation = (float)Math.PI;
+    }
+
+    if(Input.IsActionPressed("move_left")) {
+      Position += Vector2.Left * 10;
+      Rotation = (float)Math.PI;
+    }
+
+    if(Input.IsActionPressed("move_right")) {
+      Position += Vector2.Right * 10;
+      Rotation = (float)Math.PI;
+    }
   }
 }
--- World.cs
+++ World.cs
@@ -17,5 +17,6 @@ public partial class World : Node
     //     me.WrapArround();
     //   }
     // }
+    
   }
 }
