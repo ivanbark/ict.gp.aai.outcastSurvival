2997de6115046a79bb83c1a615f63ee1d97b6755
Ivan <s1169347@student.windesheim.nl>
2025-03-25 13:12:27 +0100

    Update .gitignore to exclude IDE-specific files and clean up whitespace in Guard.cs and guard.tscn

--- Entities/Guard/Guard.cs
+++ Entities/Guard/Guard.cs
@@ -4,7 +4,7 @@ using System;
 public partial class Guard : MovingEntity
 {
     public Player Player;
-    
+
     [Export]
     public float AttackCooldown;
     private float _attackCooldown;
@@ -12,12 +12,12 @@ public partial class Guard : MovingEntity
     public override void _Ready()
     {
         base._Ready();
-        
+
         AddToGroup("Entities");
-        
+
         if (World_ref != null)
             Player = World_ref.GetNode<Player>("Player");
-        
+
         _attackCooldown = 0f;
     }
 
@@ -25,23 +25,23 @@ public partial class Guard : MovingEntity
     {
         if (Player == null)
             return;
-        
+
         Vector2 desiredVelocity = SteeringBehaviour.Seek(Position, Player.Position, MaxSpeed);
         ApplyAcceleration(desiredVelocity, delta);
     }
-    
+
     public void AttackPlayer(float delta)
     {
         if (Player == null)
             return;
-        
+
         if (Position.DistanceTo(Player.Position) <= AttackRange && _attackCooldown <= 0f)
         {
             GD.Print("Attacking");
             _attackCooldown = AttackCooldown;
             Player.TakeDamage(AttackDamage);
         }
-        
+
         _attackCooldown -= delta;
     }
 }
